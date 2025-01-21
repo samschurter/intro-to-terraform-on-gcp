66cf18e1393570491684b3188bcafe89732212b4
Sam Schurter <sschurter@northpointkc.com>
2025-01-21 17:32:46 -0600

    staging and prod

--- main.tf
+++ main.tf
@@ -5,3 +5,19 @@ module "qa" {
   project_id = var.project_id
   region = var.region
 }
+
+module "staging" {
+  source = "./modules/blog"
+  app_name = "staging-blog"
+  network_name = "staging"
+  project_id = var.project_id
+  region = var.region
+}
+
+module "prod" {
+  source = "./modules/blog"
+  app_name = "prod-blog"
+  network_name = "prod"
+  project_id = var.project_id
+  region = var.region
+}
